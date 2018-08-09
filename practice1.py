5f0161ffa3270e6797a651298741a2b17cf17ea3
Monmoy042 <monmoykms.16@gmail.com>
2018-08-09 21:35:14 +0600

    practice1.py file added and modify the file

---
+++ practice1.py
@@ -0,0 +1,12 @@
+# Practice session on python
+print dir()
+# print dir(__builtins__)
+# print dir(__doc__)
+
+# Sqrt root
+import math
+print math.sqrt(25)
+
+from math import sqrt
+print sqrt(144256)
+
