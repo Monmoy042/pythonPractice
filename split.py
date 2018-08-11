70c5cd3ee302fb8e184f87a6e1ecb1a89b859980
Monmoy042 <monmoykms.16@gmail.com>
2018-08-11 11:16:45 +0600

    split.py file added and performed on split method

---
+++ split.py
@@ -0,0 +1,14 @@
+# Split in python
+s = "a b c d"
+print s.split()
+
+t = "a  b    c d"
+print t.split()
+
+me = "I like Python!"
+print me.split(" ")
+
+p = "a+b+c"
+print p.split()
+print p.split("+")
+
